524fdf40e6b9e51782918a8d54f8f4c4a330d12a
moraisLuismNet <morais.luism.net@gmail.com>
2025-12-03 21:22:30 +0100

    03.12.2025__

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://moraisluismnet.github.io',
+  site: 'https://moraisluismnet.github.io/moraisLuismNet_Portfolio/',
   base: '/moraisLuismNet_Portfolio',
   output: 'static'
 });
